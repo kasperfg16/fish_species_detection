051a492aead10d2c16153ea92afd9835e4fafc43
Kasper Grøntved <kasperfg16@outlook.dk>
2022-07-07 12:52:31 +0200

    thing

--- README.md
+++ README.md
@@ -3,29 +3,36 @@ Repo for fish species detection project at Aalborg University
 
 ## Setup
 
-### Create a conda enviroment
+1. Open a terminal
 
+    ### Create a conda enviroment
 
-Create a Conda environment using:
+    Create a Conda environment using:
 
-``` bash
-conda create -n env_pytorch python=3.6
-```
+    ``` bash
+    conda create -n env_pytorch python=3.6
+    ```
 
-Activate the environment using:
+    Activate the environment using:
 
-``` bash
-conda activate env_pytorch
-```
+    ``` bash
+    conda activate env_pytorch
+    ```
 
-### Install pytorch
+    ### Install pytorch
 
-Install PyTorch using conda:
+    Install PyTorch using conda:
 
-``` bash
-conda install pytorch torchvision -c pytorch
-```
+    ``` bash
+    conda install pytorch torchvision -c pytorch
+    ```
 
 ## Inspiration
 
-1. https://docs.microsoft.com/en-us/learn/modules/intro-computer-vision-pytorch/2-image-data
+1. loading of images:
+https://ryanwingate.com/intro-to-machine-learning/deep-learning-with-pytorch/loading-image-data-into-pytorch/
+
+2. https://docs.microsoft.com/en-us/learn/modules/intro-computer-vision-pytorch/2-image-data
+
+3. Using a pretrained model to perform image classification
+https://github.com/LeanManager/PyTorch_Image_Classifier/blob/master/train.py
--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+import model_functions
 
-from torchvision import datasets, transforms
+from torchvision import datasets, transforms, models
 from torchvision.transforms import ToTensor
+from collections import OrderedDict
+from torch import nn
+from torch import optim
+
+parser = argparse.ArgumentParser(description='Train Image Classifier')
 
 def imshow(image, ax=None, title=None, normalize=True):
     """Imshow for Tensor."""
@@ -30,6 +37,16 @@ def imshow(image, ax=None, title=None, normalize=True):
 
     return ax
 
+# Command line arguments
+parser.add_argument('--arch', type = str, default = 'vgg', help = 'NN Model Architecture')
+parser.add_argument('--learning_rate', type = float, default = 0.001, help = 'Learning Rate')
+parser.add_argument('--hidden_units', type = int, default = 10000, help = 'Neurons in the Hidden Layer')
+parser.add_argument('--epochs', type = int, default = 20, help = 'Epochs')
+parser.add_argument('--gpu', type = str, default = 'cuda', help = 'GPU or CPU')
+parser.add_argument('--save_dir', type = str, default = 'checkpoint.pth', help = 'Path to checkpoint')
+
+arguments = parser.parse_args()
+
 # Load in data
 transform = transforms.Compose([transforms.Resize(255),
                                 transforms.CenterCrop(224),
@@ -46,7 +63,7 @@ for images, labels in dataloader:
 # Get one batch
 images, labels = next(iter(dataloader))
 
-data_dir = 'loading-image-data-into-pytorch/Cat_Dog_data'
+data_dir = 'cod_and_others_training_data'
 
 transform = transforms.Compose([transforms.Resize(255),
                                 transforms.CenterCrop(224),
@@ -59,24 +76,73 @@ dataloader = torch.utils.data.DataLoader(dataset,
 
 data_dir = 'loading-image-data-into-pytorch/Cat_Dog_data'
 
-train_transforms = transforms.Compose([transforms.RandomRotation(30),
-                                       transforms.RandomResizedCrop(224),
-                                       transforms.RandomHorizontalFlip(),
-                                       transforms.ToTensor(),
-                                       transforms.Normalize([0.5, 0.5, 0.5], 
-                                                            [0.5, 0.5, 0.5])])
-test_transforms = transforms.Compose([transforms.Resize(255),
-                                      transforms.CenterCrop(224),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize([0.5, 0.5, 0.5], 
-                                                           [0.5, 0.5, 0.5])])
+train_transforms = transforms.Compose([
+        transforms.RandomRotation(30),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], 
+        [0.5, 0.5, 0.5])])
+
+test_transforms = transforms.Compose([
+        transforms.Resize(255),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], 
+        [0.5, 0.5, 0.5])])
+
+validate_transforms = transforms.Compose([
+        transforms.Resize(255),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], 
+        [0.5, 0.5, 0.5])])
 
 train_data = datasets.ImageFolder(data_dir + '/train', 
                                   transform=train_transforms)
 test_data = datasets.ImageFolder(data_dir + '/test', 
+                                 transform=test_transforms)
+validate_data = datasets.ImageFolder(data_dir + '/validate', 
                                  transform=test_transforms)
 
 trainloader = torch.utils.data.DataLoader(train_data, 
                                           batch_size=32)
 testloader = torch.utils.data.DataLoader(test_data, 
                                          batch_size=32)
+validateloader = torch.utils.data.DataLoader(validate_data, 
+                                         batch_size=32)
+
+# Build and train the neural network (Transfer Learning)
+if arguments.arch == 'vgg':
+    input_size = 25088
+    model = models.vgg16(pretrained=True)
+elif arguments.arch == 'alexnet':
+    input_size = 9216
+    model = models.alexnet(pretrained=True)
+
+print(model)
+
+# Freeze pretrained model parameters to avoid backpropogating through them
+for parameter in model.parameters():
+    parameter.requires_grad = False
+
+# Build custom classifier
+classifier = nn.Sequential(OrderedDict([('fc1', nn.Linear(input_size, arguments.hidden_units)),
+                                        ('relu', nn.ReLU()),
+                                        ('drop', nn.Dropout(p=0.5)),
+                                        ('fc2', nn.Linear(arguments.hidden_units, 102)),
+                                        ('output', nn.LogSoftmax(dim=1))]))
+
+model.classifier = classifier
+
+# Loss function (since the output is LogSoftmax, we use NLLLoss)
+criterion = nn.NLLLoss()
+
+# Gradient descent optimizer
+optimizer = optim.Adam(model.classifier.parameters(), lr=arguments.learning_rate)
+    
+model_functions.train_classifier(model, optimizer, criterion, arguments.epochs, trainloader, validate_loader, arguments.gpu)
+    
+model_functions.test_accuracy(model, trainloader, arguments.gpu)
+
+model_functions.save_checkpoint(model, train_data, arguments.arch, arguments.epochs, arguments.learning_rate, arguments.hidden_units, input_size)  
