157e3c6f9c0b8998225eb12877179e12b3896661
Kasper Grøntved <kasperfg16@outlook.dk>
2022-07-07 12:07:40 +0200

    image dataset

--- main.py
+++ main.py
@@ -4,24 +4,79 @@ import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
 
+from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor
 
-data_train = torchvision.datasets.MNIST('./data',
-        download=True,train=True,transform=ToTensor())
-data_test = torchvision.datasets.MNIST('./data',
-        download=True,train=False,transform=ToTensor())
+def imshow(image, ax=None, title=None, normalize=True):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    image = image.numpy().transpose((1, 2, 0))
 
-fig,ax = plt.subplots(1,7)
-for i in range(7):
-    ax[i].imshow(data_train[i][0].view(28,28))
-    ax[i].set_title(data_train[i][1])
-    ax[i].axis('off')
+    if normalize:
+        mean = np.array([0.5, 0.5, 0.5])
+        std = np.array([0.5, 0.5, 0.5])
+        image = std * image + mean
+        image = np.clip(image, 0, 1)
 
-print('Training samples:',len(data_train))
-print('Test samples:',len(data_test))
+    ax.imshow(image)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.tick_params(axis='both', length=0)
+    ax.set_xticklabels('')
+    ax.set_yticklabels('')
 
-print('Tensor size:',data_train[0][0].size())
-print('First 10 digits are:', [data_train[i][1] for i in range(10)])
+    return ax
 
-print('Min intensity value: ',data_train[0][0].min().item())
-print('Max intensity value: ',data_train[0][0].max().item())
+# Load in data
+transform = transforms.Compose([transforms.Resize(255),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor()])
+
+dataset = datasets.ImageFolder('images', transform=transform)
+
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+
+# Looping through it, get a batch on each loop 
+for images, labels in dataloader:
+    pass
+
+# Get one batch
+images, labels = next(iter(dataloader))
+
+data_dir = 'loading-image-data-into-pytorch/Cat_Dog_data'
+
+transform = transforms.Compose([transforms.Resize(255),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor()])
+dataset = datasets.ImageFolder(data_dir, 
+                               transform=transform)
+dataloader = torch.utils.data.DataLoader(dataset, 
+                                         batch_size=32, 
+                                         shuffle=True)
+
+data_dir = 'loading-image-data-into-pytorch/Cat_Dog_data'
+
+train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.5, 0.5, 0.5], 
+                                                            [0.5, 0.5, 0.5])])
+test_transforms = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.5, 0.5, 0.5], 
+                                                           [0.5, 0.5, 0.5])])
+
+train_data = datasets.ImageFolder(data_dir + '/train', 
+                                  transform=train_transforms)
+test_data = datasets.ImageFolder(data_dir + '/test', 
+                                 transform=test_transforms)
+
+trainloader = torch.utils.data.DataLoader(train_data, 
+                                          batch_size=32)
+testloader = torch.utils.data.DataLoader(test_data, 
+                                         batch_size=32)
