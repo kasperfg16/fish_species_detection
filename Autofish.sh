061fa7fc6628c7fdaf9ab25ba87bf61665503e46
kasperfg16 <kasperfg16@outlook.dk>
2022-08-16 12:37:38 +0200

    sh ting

--- Autofish.sh
+++ Autofish.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kgrant19@student.aau.dk
 #SBATCH --job-name Autofish
-#SBATCH --time 24:00:00
+#SBATCH --time 100:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --qos=normal
 srun python train_k_fold_val.py
